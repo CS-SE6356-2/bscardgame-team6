54148a8a38ef21a0caa244269eb009eb0b88ae73
alintawang <alinta.wang@gmail.com>
2019-04-08 11:48:52 -0500

    Add files via upload
    Added Card (with FaceValue and CardSuit), Game, DiscardPile classes

---
+++ Card.java
@@ -0,0 +1,23 @@
+
+
+public class Card {
+
+	public enum faceValue {ACE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING}
+	public enum cardSuit {CLUBS, DIAMONDS, HEARTS, SPADES}
+	faceValue faceValue;
+	cardSuit cardSuit;
+	
+	public Card(faceValue f, cardSuit s) {
+		faceValue = f;
+		cardSuit = s;
+	}
+	
+	public faceValue getFaceValue() {
+		return faceValue;
+	}
+	
+	public cardSuit getCardSuit() {
+		return cardSuit;
+	}
+
+}
