061bd1b64dcde1903efbadce3eee21c78ed8dbc3
Arya Ayati <39654865+Arya_Ayati@users.noreply.github.com>
2019-04-17 20:07:05 -0500

    updated client communication file

--- BSCardGameClient/src/bscardgameclient/BSServerCommunication.java
+++ BSCardGameClient/src/bscardgameclient/BSServerCommunication.java
@@ -1,16 +1,29 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package bscardgameclient;
 
-/**
- *
- * @author shravanjambukesan
- */
-public class BSServerCommunication 
-{
-    public String text1;
-    public String text2;
+import java.util.ArrayList;
+
+public class BSServerCommunication {
+    Integer lobby;
+    int numPlayers;
+    boolean started;
+    String confirmR = "Recieved"; //reciept confirmation
+    
+    int currentTurn;
+    int previousTurn;
+    ArrayList[] PlayerHands;
+    boolean emptyPile;
+    
+    int actor;//player number
+    int action;//what they did: 0 is play a card, 1 is challenge, 2 is win
+    ArrayList<Integer> cardsPlayed;
+    
+    
+    
+    BSServerCommunication(Integer lobbycode)
+    {
+	lobby = lobbycode;
+	PlayerHands = new ArrayList[6];
+	cardsPlayed = new ArrayList<>();
+	started = false;
+    }
 }
